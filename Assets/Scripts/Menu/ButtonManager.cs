d20619a771e648b469d004f13e50d151b7158730
tylkoszinso <michal222.091@gmail.com>
2024-01-27 17:43:45 +0100

    added basic menu

---
+++ Assets/Scripts/Menu/ButtonManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void StartGameFake()
+    {
+        Application.Quit();
+    }
+
+    public void DontStartGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
